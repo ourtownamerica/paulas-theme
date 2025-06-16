362e27bf7fad52b7cf33d5f2619a8a0661bb9616
Pamblam <adelphia@protonmail.ch>
2025-06-16 08:06:40 -0400

    saving

--- assets/php/enqueue_scripts.php
+++ assets/php/enqueue_scripts.php
@@ -5,30 +5,31 @@
  * @return void
  */
 function paulas_theme_enqueue_scripts() {
-
+	$theme = wp_get_theme();
+	
 	// Load Bootstrap on every page
 	$bootstrap_path = TEMPLATE_DIR_URI.'/assets/js/bootstrap.bundle.js';
-	wp_enqueue_script('bootstrap-js', $bootstrap_path, [], @filemtime($bootstrap_path), true);
+	wp_enqueue_script('bootstrap-js', $bootstrap_path, [], $theme->get('Version'), true);
 
 	// Load aos js on every page
 	$aosjs_path = TEMPLATE_DIR_URI.'/assets/js/aos.js';
-	wp_enqueue_script('aos-js', $aosjs_path, [], @filemtime($aosjs_path), true);
+	wp_enqueue_script('aos-js', $aosjs_path, [], $theme->get('Version'), true);
 
 	wp_enqueue_script('jquery');
 
 	// Load aos js on every page
 	$slickjs_path = TEMPLATE_DIR_URI.'/assets/js/slick.min.js';
-	wp_enqueue_script('slick-js', $slickjs_path, ['jquery'], @filemtime($slickjs_path), true);
+	wp_enqueue_script('slick-js', $slickjs_path, ['jquery'], $theme->get('Version'), true);
 
 	// Load main js on every page
 	$mainjs_path = TEMPLATE_DIR_URI.'/assets/js/index.js';
-	wp_enqueue_script('index-js', $mainjs_path, ['bootstrap-js', 'aos-js', 'jquery', 'slick-js'], @filemtime($mainjs_path), true);
+	wp_enqueue_script('index-js', $mainjs_path, ['bootstrap-js', 'aos-js', 'jquery', 'slick-js'], $theme->get('Version'), true);
 	wp_localize_script('index-js', 'wpData', [
 		'templateUrl' => TEMPLATE_DIR_URI
 	]);
 
 	// Load admin js on admin pages
 	$admin_js = TEMPLATE_DIR_URI.'/assets/js/admin.js';
-	wp_enqueue_script('my-admin-js', $admin_js, [], null, true);
+	wp_enqueue_script('my-admin-js', $admin_js, [], $theme->get('Version'), true);
 
 }
--- assets/php/enqueue_styles.php
+++ assets/php/enqueue_styles.php
@@ -5,34 +5,35 @@
  * @return void
  */
 function paulas_theme_enqueue_styles(){
+	$theme = wp_get_theme();
 
 	// Load Bootstrap on every page
 	$bootstrap_path = TEMPLATE_DIR_URI.'/assets/css/bootstrap.min.css';
-	wp_enqueue_style('bootstrap', $bootstrap_path, [], @filemtime($bootstrap_path));
+	wp_enqueue_style('bootstrap', $bootstrap_path, [], $theme->get('Version'));
 
 	// Load fonts css
 	$fonts_path = TEMPLATE_DIR_URI.'/assets/css/fonts.css';
-	wp_enqueue_style('fonts', $fonts_path, [], @filemtime($fonts_path));
+	wp_enqueue_style('fonts', $fonts_path, [], $theme->get('Version'));
 
 	// Load fontawesome css
 	$fontawesome_path = TEMPLATE_DIR_URI.'/assets/fontawesome/css/all.min.css';
-	wp_enqueue_style('fontawesome', $fontawesome_path, [], @filemtime($fontawesome_path));
+	wp_enqueue_style('fontawesome', $fontawesome_path, [], $theme->get('Version'));
 
 	// Load aos css
 	$aos_path = TEMPLATE_DIR_URI.'/assets/css/aos.css';
-	wp_enqueue_style('aos', $aos_path, [], @filemtime($aos_path));
+	wp_enqueue_style('aos', $aos_path, [], $theme->get('Version'));
 
 	// Load slick css
 	$slick_path = TEMPLATE_DIR_URI.'/assets/css/slick.css';
-	wp_enqueue_style('slick', $slick_path, [], @filemtime($slick_path));
+	wp_enqueue_style('slick', $slick_path, [], $theme->get('Version'));
 
 	// Load slick css
 	$slick_theme_path = TEMPLATE_DIR_URI.'/assets/css/slick-theme.css';
-	wp_enqueue_style('slick-theme', $slick_theme_path, ['slick'], @filemtime($slick_theme_path));
+	wp_enqueue_style('slick-theme', $slick_theme_path, ['slick'], $theme->get('Version'));
 
 	// Load main theme css
 	$theme_path = TEMPLATE_DIR_URI.'/assets/css/paulas_theme.css';
-	wp_enqueue_style('paulas_theme', $theme_path, ['bootstrap', 'fonts'], @filemtime($theme_path));
+	wp_enqueue_style('paulas_theme', $theme_path, ['bootstrap', 'fonts'], $theme->get('Version'));
 
 
 }
